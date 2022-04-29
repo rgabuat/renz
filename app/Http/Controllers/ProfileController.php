9791087a9a5106ffef0a0367974d04abfc486c96
rgabuat <johncarlgabuat@gmail.com>
2022-04-30 05:58:24 +0800

    558

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -33,28 +33,54 @@ class ProfileController extends Controller
 
     public function update(Request $request,$uid)
     {
-        // $this->validate($request, [
-        //     'company' => 'required|max:255',
-        //     'firstname' => 'required|max:255',
-        //     'lastname' => 'required|max:255',
-        //     'address' => 'required|max:255',
-        //     'phone_number' => 'required|max:255',
-        //     'username' => 'required|max:255',
-        //     'email' => 'required|email|max:255',
-        //     'password' => 'required',
-        //     'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-        // ]);
-        
+        $this->validate($request, [
+            'firstname' => 'required|max:255',
+            'lastname' => 'required|max:255',
+            'address' => 'required|max:255',
+            'phone_number' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'image' => '|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+        ]);
+
+        if(auth()->user()->hasRole('company admin|company user'))
+        {
+            $param = [
+                'company_name' => $request->company
+            ];
+        }
+
+        $param = [
+            'first_name' => $request->firstname,
+            'last_name' => $request->lastname,
+            'address' => $request->address,
+            'phone_number' => $request->phone_number,
+            'username' => $request->username,
+            'email' => $request->email
+        ];
+
         if($request->has('image'))
         {
             $destinationPath = 'images/uploads';
             $file = $request->file('image');
             $file_name = $file->getClientOriginalName();
             $path = $request->file('image')->storeAs($destinationPath,$file_name);
-            
-            $update_profile = User::where('id',$uid)->update(['profile_image' => $path]);
+            $param = [
+                'profile_image' => $path
+            ];
         }
-        return redirect()->back()->with('status', 'Profile Successfully Update');
+
+        $update_profile = User::where('id',$uid)->update($param);
+
+        if($update_profile)
+        {
+            return redirect()->back()->with('status', 'Profile Successfully Update');
+        }
+        else 
+        {
+            return redirect()->back()->with('status', 'Something went wrong!');
+        }
+        
     }
     
 }
